634ad86b2fa2e73093e0c86cd7aa55d8989bdab6
quanghaubunho-del <quanghaubunho@gmail.com>
2025-10-29 16:48:10 +0700

    Update config.php

--- config/config.php
+++ config/config.php
@@ -1,10 +1,10 @@
 <?php
 $configDB = array();
-$configDB["host"] 		= "web1.mysql.database.azure.com";
+$configDB["host"] 		= "webhau1.mysql.database.azure.com";
 $configDB["database"]	= "computer_store";
 $configDB["username"] 	= "aovhau";
 $configDB["password"] 	= "Hau@2005";
-define("HOST", "web1.mysql.database.azure.com");
+define("HOST", "webhau1.mysql.database.azure.com");
 define("DB_NAME", "computer_store");
 define("DB_USER", "aovhau");
 define("DB_PASS", "Hau@2005");
